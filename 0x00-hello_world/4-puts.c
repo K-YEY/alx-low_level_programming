ebb5e1fa6d1a187cdf814b8535b2c18cffa28030
K-YEY <KNIGHT@gmail.com>
2023-06-15 05:44:23 -0700

    Puts C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
 #include <stdio.h>
-int main(void){puts("\"Programming is like building a multilingual puzzle");return (0);}
+/* main -> puts -> True */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
