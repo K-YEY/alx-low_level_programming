60bf8637a6c0631152270613f31de6f619035298
K-YEY <KNIGHT@gmail.com>
2023-06-20 00:12:57 -0700

    PutChar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Check Main
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: Nothing.
+ */
+
+void print_alphabet_x10(void)
+{
+	char a;
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
